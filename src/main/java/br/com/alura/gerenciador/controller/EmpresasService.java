f5e5d32baf6310aec1e0913deebcd05400234e4e
vieiragranzoto <vieiragranzoto@gmail.com>
2022-12-11 11:13:47 -0300

    feature

--- src/main/java/br/com/alura/gerenciador/controller/EmpresasService.java
+++ src/main/java/br/com/alura/gerenciador/controller/EmpresasService.java
@@ -12,19 +12,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.XStreamer;
-import com.thoughtworks.xstream.converters.time.LocalDateConverter;
 
 import br.com.alura.gerenciador.model.Banco;
 import br.com.alura.gerenciador.model.Empresa;
 
 @WebServlet("/empresas")
-public class ListaEmpresaService extends HttpServlet {
+public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
@@ -32,23 +32,32 @@ public class ListaEmpresaService extends HttpServlet {
 
 		List<Empresa> empresas = new Banco().getListaEmpresa();
 
-		XStream xStream = new XStream();	
+		String valor = "";
 		
-		xStream.alias("empresa", Empresa.class);
-		
-		String xml = xStream.toXML(empresas);
+		if(request.getHeader("Accepet")!=null)
+			valor = request.getHeader("Accepet");
 
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
+		if (valor.contains("xml")) {
 
-//		Gson gson = new GsonBuilder()
-//		        .setPrettyPrinting()
-//		        .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
-//		        .create();
-//		String json = gson.toJson(empresas);
-//		
-//		response.setContentType("application/json");
-//		response.getWriter().print(json);
+			XStream xStream = new XStream();
+			xStream.alias("empresa", Empresa.class);
+			String xml = xStream.toXML(empresas);
+
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+
+		} else if (valor.contains("json")) {
+
+			Gson gson = new GsonBuilder().setPrettyPrinting()
+					.registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'no content'}");
+		}
 	}
 
 }
---
+++ src/test/java/br/com/alura/gerenciador/controller/EntradaServletTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.gerenciador.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntradaServletTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
